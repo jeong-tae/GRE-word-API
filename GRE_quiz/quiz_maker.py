4f4050db108b208e3765627f40c30d284ef8ed2c
star0505 <star0505@github.com>
2019-09-29 21:25:05 +0900

    make_message_output

--- GRE_quiz/quiz_maker.py
+++ GRE_quiz/quiz_maker.py
@@ -3,90 +3,109 @@ import random
 import glob
 
 class quiz_maker(object):
-	def __init__(self, DATA_PATH):
-		self.wordset = dict()
-		for data_path in glob.glob("%s/*" % DATA_PATH):
-			with open(data_path) as f:
-				for line in f.readlines():
-					word = line.rstrip().split("\t")[0]
-					if word in self.wordset:
-						self.wordset[word].append(line.rstrip().split("\t"))
-					else:
-						self.wordset[word] = [line.rstrip().split("\t")]
-		self.word_list = self.wordset.keys()
-		self.sample_size = 5
+    def __init__(self, DATA_PATH):
+        self.wordset = dict()
+        for data_path in glob.glob("%s/*" % DATA_PATH):
+            with open(data_path) as f:
+                for line in f.readlines():
+                    word = line.rstrip().split("\t")[0]
+                    if word in self.wordset:
+                        self.wordset[word].append(line.rstrip().split("\t"))
+                    else:
+                        self.wordset[word] = [line.rstrip().split("\t")]
+        self.word_list = self.wordset.keys()
+        self.sample_size = 5
+        self.question_type = ""
+        self.field = 2
 
-	
-	def jaccard_sim(self, w1, w2):
-		w1 = w1.replace(" ", "").replace(",", "").replace(";", "")
-		w2 = w2.replace(" ", "").replace(",", "").replace(";", "")
-		ch1 = list(filter(lambda x: x in w2, w1))
-		ch2 = list(filter(lambda x: x in w1, w2))
-		js1 = len(ch1)/len(w1)
-		js2 = len(ch2)/len(w2)
-		return js1*js2
+    def update_question_type(self, question_type):
+        self.question_type = question_type
+        if self.question_type == "cloze": 
+            self.sample_size = 2
+            self.field = 3
+        elif self.question_type == "synonyms":
+            self.sample_size = 2
+            self.field = 4
 
-	def select_candid(self, word):
-		return sorted(self.word_list, key=lambda x: self.jaccard_sim(word, x), reverse=True)
+    def jaccard_sim(self, w1, w2):
+        w1 = w1.replace(" ", "").replace(",", "").replace(";", "")
+        w2 = w2.replace(" ", "").replace(",", "").replace(";", "")
+        ch1 = list(filter(lambda x: x in w2, w1))
+        ch2 = list(filter(lambda x: x in w1, w2))
+        js1 = len(ch1)/len(w1)
+        js2 = len(ch2)/len(w2)
+        return js1*js2
 
-	def random_sampling(self, target_index=False):
-		result = []
-		if target_index: 
-			data = filter(lambda a: a!=False, map(lambda x: x if target_index in x else False, self.wordset))
-		else: 
-			data = self.wordset
-		for i, sample in enumerate(data):
-			if i < self.sample_size: 
-				result.append(sample)
-			else:
-				r = random.randint(0, i)
-				if r < self.sample_size: 
-					result[r] = sample
-				else:
-					pass
-		return result
+    def select_candid(self, word):
+        return sorted(self.word_list, key=lambda x: self.jaccard_sim(word, x), reverse=True)
+    
+    def filter_by_field(self, data):
+        filtered = []
+        for word in data:
+            sample_list = self.wordset[word]
+            for sample in sample_list:
+                app = False
+                if len(sample) < self.field: continue
+                if sample[self.field-1] == "": continue
+                app = True
+            if app: filtered.append(word)
+        return filtered
 
-	def create_candid_pool(self):
-		candidates_pool = dict()
-		for sample in self.random_sampling():
-			info = random.choice(self.wordset[sample])
-			meaning = info[1]
-			candidates = self.select_candid(sample)
-			i = 0
-			candidates_pool[sample] = []
-			for candidate in candidates:
-				meaning_cand = list(map(lambda x: x[1], self.wordset[candidate]))
-				s = max(map(lambda x: self.jaccard_sim(meaning.replace("하다",""), x.replace("하다","")), meaning_cand))
-				if s > 0.3: 
-					pass
-				else:
-					candidates_pool[sample].append(candidate)
-					i += 1
-				if i >= 4: break
-		return candidates_pool
+    def random_sampling(self):
+        result = []
+        if self.field >=3 :
+            data = self.filter_by_field(self.word_list)
+        else: 
+            data = self.word_list
+        for i, sample in enumerate(data):
+            if i < self.sample_size: 
+                result.append(sample)
+            else:
+                r = random.randint(0, i)
+                if r < self.sample_size: 
+                    result[r] = sample
+                else:
+                    pass
+        return result
 
-	def make_quiz_meaning(self, candidate_dict):
-		answer, candidates = candidate_dict
-		meaning = ";".join(map(lambda x: x[1], self.wordset[answer]))
-		wrong_answer_list = list(map(lambda x: (x, random.choice(self.wordset[x])[1]), candidates))
-		return answer, meaning, wrong_answer_list
+    def create_candid_pool(self):
+        candidates_pool = dict()
+        for sample in self.random_sampling():
+            info = random.choice(self.wordset[sample])
+            meaning = info[1]
+            candidates = self.select_candid(sample)
+            i = 0
+            candidates_pool[sample] = []
+            for candidate in candidates:
+                meaning_cand = list(map(lambda x: x[1], self.wordset[candidate]))
+#                s = max(map(lambda x: self.jaccard_sim(meaning.replace("하다",""), x.replace("하다","")), meaning_cand))
+#               if s > 0.3: 
+#                  pass
+#               else:
+                candidates_pool[sample].append(candidate)
+                i += 1
+                if i >= 4: break
+        return candidates_pool
 
-	def make_quiz_cloze(self, candidate_dict):
-		answer, candidates = candidate_dict
-		meaning = ";".join(map(lambda x: x[1], self.wordset[answer]))
-		if self.wordset[answer][2] == "": 
-			return
-		else:
-			ex_sent = self.wordset[answer][2].replace(answer, "(	)")
-			wrong_answer_list = list(map(lambda x: (x, random.choice(self.wordset[x])[1]), candidates))
-			return answer, meaning, wrong_answer_list, ex_sent
+    def make_quiz_meaning(self, candidate_dict):
+        word, candidates = candidate_dict
+        meaning = ";".join(map(lambda x: x[1], self.wordset[word]))
+        wrong_answer_list = list(map(lambda x: (x, random.choice(self.wordset[x])[1]), candidates))
+        target_info = (word, meaning)
+        return target_info, wrong_answer_list, ""
 
-	def make_quiz_synonym(self, candidate_dict):
-		answer, candidates = candidate_dict
-		meaning = ";".join(map(lambda x: x[1], self.wordset[answer]))
-		if self.wordset[answer][3] == "": 
-			return
-		else:
-			synonyms = self.wordset[answer][3]
-			wrong_answer_list = list(map(lambda x: (x, random.choice(self.wordset[x])[1]), candidates))
-			return answer, meaning, wrong_answer_list, synonyms
+    def make_quiz_cloze(self, candidate_dict):
+        word, candidates = candidate_dict
+        meaning = ";".join(map(lambda x: x[1], self.wordset[word]))
+        ex_sent = random.choice(self.wordset[word])[2].replace(word, "(    )")
+        wrong_answer_list = list(map(lambda x: (x, random.choice(self.wordset[x])[1]), candidates))
+        target_info = (word, meaning)
+        return target_info, wrong_answer_list, ex_sent
+
+    def make_quiz_synonyms(self, candidate_dict):
+        word, candidates = candidate_dict
+        meaning = ";".join(map(lambda x: x[1], self.wordset[word]))
+        target_info = (word, meaning)
+        synonyms = random.choice(self.wordset[word])[3]
+        wrong_answer_list = list(map(lambda x: (x, random.choice(self.wordset[x])[1]), candidates))
+        return target_info, wrong_answer_list, synonyms
--- send_quiz.py
+++ send_quiz.py
@@ -7,49 +7,59 @@ import time
 token = os.environ["SLACK_BOT_TOKEN"]
 slack = Slacker(token)
 quiz_maker = quiz_maker("GRE_quiz/data")
-moreInfo = ['person', 'person1', 'person2']
-candidates = quiz_maker.create_candid_pool()
 question_type = sys.argv[1]
+quiz_maker.update_question_type(question_type)
 
-def make_action_form(target, candid, text_type):
-    msg_qa = ("Correct!", "#339CFF") if candid[0] == target[0] else ("Wrong!", "#FF4B33")
-    if text_type == "word": 
-        candid_text = candid[0]
-    elif text_type == "meaning": 
-        candid_text = candid[1]
-    else: 
-        return
+def make_title_text(target, q_type, question=None):
+    if q_type is "word":
+        return target[0]
+    elif q_type is "meaning":
+        return target[1]
+    elif q_type in ["cloze", "synonyms"]:
+        return question 
+
+def make_action_form(target, candid, q_type):
+    if q_type == "meaning": 
+        idx = 1
+    else:
+        idx = 0
+    msg_qa = ("Correct!", "#339CFF") if candid[idx] == target[idx] else ("Wrong!", "#FF4B33")
     msg_cand_mean = {
-            "name": text_type,
+            "name": q_type,
             "type": "button",
-            "text": candid_text,
+            "text": candid[idx],
             "style": "primary",
-            "value": candid_text,
+            "value": candid[idx],
             "confirm": {
-                "title": msg_qa[0],
-                "color": msg_qa[1], 
+                "title": msg_qa[idx],
+                "color": msg_qa[idx], 
                 "text": "    ".join(candid_info),
                 }
             }
     return msg_cand_mean
 
+if question_type in ["word", "meaning"]:
+    make_quiz_ft = quiz_maker.make_quiz_meaning
+elif question_type == "cloze":
+    make_quiz_ft = quiz_maker.make_quiz_cloze
+elif question_type == "synonyms":
+    make_quiz_ft = quiz_maker.make_quiz_synonyms
+else:
+    raise(ValueError, "Type the question type: word, meaning, cloze, synonyms")
+
+candidates = quiz_maker.create_candid_pool()
+
 for candidate in candidates.items():
-    target_word, meaning, wrong_answer_list = quiz_maker.make_quiz_meaning(candidate)
-    target_info = [target_word, meaning]
-    quiz_candids = set(map(lambda x: (x[0],x[1]), wrong_answer_list))
-    quiz_candids.add((target_word,meaning))
-    print(quiz_candids)
     message = dict()
-    if question_type == "word":
-        message["title"] = meaning
-    elif question_type == "meaning":
-        message["title"] = target_word
+    target_info, wrong_answer_list, question = make_quiz_ft(candidate)
+    quiz_candids = set(map(lambda x: (x[0],x[1]), wrong_answer_list))
+    quiz_candids.add(target_info)
+    message["title"] = make_title_text(target_info, question_type, question=question)
     message["actions"] = []
-    for q_candid_w, q_candid_m in quiz_candids:
-        candid_info = [q_candid_w, q_candid_m]
-        msg_cand_mean = make_action_form(target_info, candid_info, text_type=question_type)
+    for candid_info in quiz_candids:
+        msg_cand_mean = make_action_form(target_info, candid_info, question_type)
         message["actions"].append(msg_cand_mean)
     clock = time.localtime(time.time())
     current_time = "%s-%s-%s-%s:%s" % (clock.tm_year,clock.tm_mon,clock.tm_mday,clock.tm_hour,clock.tm_min)
-    slack.chat.post_message("#education-study", '[%s] GRE Daily Quiz - %s' % (current_time, question_type.upper()), attachments=[message])
+    slack.chat.post_message("#daily_quiz", '[%s] GRE Daily Quiz - %s' % (current_time, question_type.upper()), attachments=[message])
     break
