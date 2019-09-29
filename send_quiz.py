9abbf605c674feae4e09692d08697a853588d384
star0505 <star0505@github.com>
2019-09-29 22:41:03 +0900

    functionize

--- send_quiz.py
+++ send_quiz.py
@@ -7,14 +7,12 @@ import time
 token = os.environ["SLACK_BOT_TOKEN"]
 slack = Slacker(token)
 quiz_maker = quiz_maker("GRE_quiz/data")
-question_type = sys.argv[1]
-quiz_maker.update_question_type(question_type)
 
 def make_title_text(target, q_type, question=None):
-    if q_type is "word":
-        return target[0]
-    elif q_type is "meaning":
+    if q_type == "word":
         return target[1]
+    elif q_type == "meaning":
+        return target[0]
     elif q_type in ["cloze", "synonyms"]:
         return question 
 
@@ -28,38 +26,47 @@ def make_action_form(target, candid, q_type):
             "name": q_type,
             "type": "button",
             "text": candid[idx],
-            "style": "primary",
+            "style": "default",
             "value": candid[idx],
             "confirm": {
-                "title": msg_qa[idx],
-                "color": msg_qa[idx], 
-                "text": "    ".join(candid_info),
+                "title": msg_qa[0],
+                "color": msg_qa[1], 
+                "text": "    ".join(candid),
+                "dismisss_text": "Skip"
                 }
             }
     return msg_cand_mean
 
-if question_type in ["word", "meaning"]:
-    make_quiz_ft = quiz_maker.make_quiz_meaning
-elif question_type == "cloze":
-    make_quiz_ft = quiz_maker.make_quiz_cloze
-elif question_type == "synonyms":
-    make_quiz_ft = quiz_maker.make_quiz_synonyms
-else:
-    raise(ValueError, "Type the question type: word, meaning, cloze, synonyms")
+def make_message(question_type):
+    quiz_maker.update_question_type(question_type)
 
-candidates = quiz_maker.create_candid_pool()
 
-for candidate in candidates.items():
-    message = dict()
-    target_info, wrong_answer_list, question = make_quiz_ft(candidate)
-    quiz_candids = set(map(lambda x: (x[0],x[1]), wrong_answer_list))
-    quiz_candids.add(target_info)
-    message["title"] = make_title_text(target_info, question_type, question=question)
-    message["actions"] = []
-    for candid_info in quiz_candids:
-        msg_cand_mean = make_action_form(target_info, candid_info, question_type)
-        message["actions"].append(msg_cand_mean)
-    clock = time.localtime(time.time())
-    current_time = "%s-%s-%s-%s:%s" % (clock.tm_year,clock.tm_mon,clock.tm_mday,clock.tm_hour,clock.tm_min)
-    slack.chat.post_message("#daily_quiz", '[%s] GRE Daily Quiz - %s' % (current_time, question_type.upper()), attachments=[message])
-    break
+    if question_type in ["word", "meaning"]:
+        make_quiz_ft = quiz_maker.make_quiz_meaning
+    elif question_type == "cloze":
+        make_quiz_ft = quiz_maker.make_quiz_cloze
+    elif question_type == "synonyms":
+        make_quiz_ft = quiz_maker.make_quiz_synonyms
+    else:
+        raise(ValueError, "Type the question type: word, meaning, cloze, synonyms")
+
+    candidates = quiz_maker.create_candid_pool()
+
+    for candidate in candidates.items():
+        message = dict()
+        target_info, wrong_answer_list, question = make_quiz_ft(candidate)
+        quiz_candids = set(map(lambda x: (x[0],x[1]), wrong_answer_list))
+        quiz_candids.add(target_info)
+        message["title"] = "*%s*" % make_title_text(target_info, question_type, question=question)
+        message["actions"] = []
+        for candid_info in quiz_candids:
+            msg_cand_mean = make_action_form(target_info, candid_info, question_type)
+            message["actions"].append(msg_cand_mean)
+        clock = time.localtime(time.time())
+        current_time = "%s-%s-%s-%s:%s" % (clock.tm_year,clock.tm_mon,clock.tm_mday,clock.tm_hour,clock.tm_min)
+        slack.chat.post_message("#test", '[%s] GRE Daily Quiz - %s' % (current_time, question_type.upper()), attachments=[message])
+        break
+
+if __name__=="__main__":
+    question_type = sys.argv[1]
+    make_message(question_type)
